28d09debcc48326b6baff09f77321c28aa49c753
David Zoufalý <davidzoufaly@gmail.com>
2019-10-31 18:07:46 +0100

    ff

---
+++ .next/static/webpack/static/development/pages/emails/[key].js.56416729880a07957d70.hot-update.js
@@ -0,0 +1,67 @@
+webpackHotUpdate("static/development/pages/emails/[key].js",{
+
+/***/ "./components/LoadingSpinner.tsx":
+/*!***************************************!*\
+  !*** ./components/LoadingSpinner.tsx ***!
+  \***************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _material_ui_core_CircularProgress__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @material-ui/core/CircularProgress */ "./node_modules/@material-ui/core/esm/CircularProgress/index.js");
+var _jsxFileName = "/Users/davidzoufaly/code/dp/crm-app-fe/components/LoadingSpinner.tsx";
+
+var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
+
+
+var LoadingSpinner = function LoadingSpinner() {
+  return __jsx(_material_ui_core_CircularProgress__WEBPACK_IMPORTED_MODULE_1__["default"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 4
+    },
+    __self: this
+  });
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (LoadingSpinner);
+
+/***/ }),
+
+/***/ "./library/globalVariables.tsx":
+/*!*************************************!*\
+  !*** ./library/globalVariables.tsx ***!
+  \*************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+var globalVars = {
+  titleSubText: "| CRM-APP",
+  // serverURL: "http://localhost:8080/api",
+  serverURL: "https://crm-app-be.herokuapp.com/api",
+  permanentFields: {
+    fistName: "First name",
+    lastName: "Last name",
+    email: "Email",
+    dateAdded: "Date added",
+    lastModified: "Last modified"
+  },
+  fieldTypes: {
+    text: "text",
+    number: "number",
+    select: "select"
+  },
+  blankOption: "---",
+  msgSuccess: "Success"
+};
+/* harmony default export */ __webpack_exports__["default"] = (globalVars);
+
+/***/ })
+
+})
+//# sourceMappingURL=[key].js.56416729880a07957d70.hot-update.js.map
